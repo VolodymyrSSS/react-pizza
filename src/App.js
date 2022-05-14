320aec5f895b8311bc7c0c1cc9652fdc8670a420
Volodymyr Sych <volodymyr_sych@epam.com>
2022-05-14 20:50:19 +0300

    using axios insted of fetch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
@@ -10,11 +11,21 @@ function App() {
   const [pizzas, setPizzas] = React.useState([]);
 
   React.useEffect(() => {
-    fetch('http://localhost:3000/db.json')
-      .then((resp) => resp.json())
-      .then((json) => {
-        setPizzas(json.pizzas);
-      });
+    // using axios
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      console.log('Axios' - data);
+      setPizzas(data.pizzas);
+    });
+
+    // using fetch
+    // fetch('http://localhost:3000/db.json')
+    //   .then((resp) => {
+    //      console.log('Fetch' - resp);
+    //      return resp.json()
+    //    })
+    //   .then((json) => {
+    //     setPizzas(json.pizzas);
+    //   });
   }, []);
 
   return (
