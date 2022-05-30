8491a02da0a4a81003f4ab86f03a24802a6fe5d2
Volodymyr Sych <volodymyr_sych@epam.com>
2022-05-30 20:20:53 +0300

    started to use middleWare thunk

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
-import axios from 'axios';
 import { useDispatch } from 'react-redux';
 
 import { Header } from './components';
 import { Home, Cart } from './pages';
 import { Routes, Route } from 'react-router-dom';
 
-import { setPizzas } from './redux/actions/pizzas';
+import { fetchPizzas } from './redux/actions/pizzas';
 
 function App () {
   const dispatch = useDispatch();
 
   React.useEffect(() => {
-    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
-      dispatch(setPizzas(data));
-    });
+    dispatch(fetchPizzas());
   }, [dispatch]);
 
   return (
--- src/redux/actions/pizzas.js
+++ src/redux/actions/pizzas.js
@@ -1,3 +1,14 @@
+import axios from 'axios';
+
+// по суті - метод для отримання та збереження піц в Редакс
+export const fetchPizzas = () => dispatch => {
+    axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+        dispatch(setPizzas(data));
+    });
+};
+
+
+// по суті - метод для збереження піц в Редакс
 export const setPizzas = items => ({
     type: 'SET_PIZZAS',
     payload: items,
